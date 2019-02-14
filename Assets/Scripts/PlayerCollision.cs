c099f4b240d75422ecba42e5baa3db111961b2fe
Tran_Daniel <trand010@hsd.k12.or.us>
2019-02-14 09:22:00 -0800

    gameplay

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public playermovement movement;
+
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
+    
